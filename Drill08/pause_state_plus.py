2ce2ecf90a4bee7da22791da93ac9b17a382f3a8
soyeong park <syys0104@gmail.com>
2019-10-21 19:32:58 +0900

    drill08_02
    2018180019 박소영
    https://youtu.be/feD0IVr86s4
    커밋횟수: 4회

---
+++ Drill08/pause_state_plus.py
@@ -0,0 +1,61 @@
+import game_framework
+import main_state
+from pico2d import *
+
+
+name = "PauseState"
+image = None
+pause = False
+
+def enter():
+    global image
+    image = load_image('pause.png')
+    pass
+
+
+def exit():
+    global image
+    del(image)
+    pass
+
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if (event.type, event.key) == (SDL_KEYDOWN, SDLK_p):
+                game_framework.pop_state()
+                pause = True
+
+    pass
+
+
+def draw():
+    global pause
+    clear_canvas()
+    image.draw(400, 300, 400, 400)
+    main_state.draw()
+    if pause == False:
+        image.draw(400, 300, 400, 400)
+    else:
+        pause = True
+    delay(0.01)
+    update_canvas()
+
+
+
+def update():
+    pass
+
+
+
+
+def resume():
+    pass
+
+
+
+
+
