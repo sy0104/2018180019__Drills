30617aba2bded2c51e7a38a4bc5a42b217969026
soyeong park <syys0104@gmail.com>
2019-10-21 18:56:11 +0900

    drill08

--- Drill08/main_state.py
+++ Drill08/main_state.py
@@ -6,6 +6,7 @@ from pico2d import *
 
 import game_framework
 import title_state
+import pause_state
 
 
 
@@ -46,11 +47,15 @@ class Boy:
 
 
 def enter():
-    pass
+    global boy, grass
+    boy = Boy()
+    grass = Grass()
 
 
 def exit():
-    pass
+    global boy, grass
+    del(boy)
+    del(grass)
 
 
 def pause():
@@ -62,15 +67,25 @@ def resume():
 
 
 def handle_events():
-    pass
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_state(title_state)
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
+            game_framework.push_state(pause_state)
 
 
 def update():
-    pass
+    boy.update()
 
 
 def draw():
-    pass
+    clear_canvas()
+    grass.draw()
+    boy.draw()
+    update_canvas()
 
 
 
