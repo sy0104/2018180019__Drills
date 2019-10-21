7401a864c03936217570dbcdad3052fc6612e4bb
soyeong park <syys0104@gmail.com>
2019-10-21 19:31:34 +0900

    drill08

--- Drill08/main_state.py
+++ Drill08/main_state.py
@@ -6,7 +6,7 @@ from pico2d import *
 
 import game_framework
 import title_state
-import pause_state
+import pause_state_plus
 
 
 
@@ -74,7 +74,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.change_state(title_state)
         elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.push_state(pause_state)
+            game_framework.push_state(pause_state_plus)
 
 
 def update():
