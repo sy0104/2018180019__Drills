19dbdf0dc04145af8a95d8c0888bb9ada8dc2a20
soyeong park <syys0104@gmail.com>
2019-11-14 20:54:33 +0900

    Update game_world.py

--- Drill11/game_world.py
+++ Drill11/game_world.py
@@ -8,6 +8,10 @@ def add_object(o, layer):
     objects[layer].append(o)
 
 
+def add_objects(l,layer):
+    objects[layer] += l
+
+
 def remove_object(o):
     for i in range(len(objects)):
         if o in objects[i]:
